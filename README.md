f3a432b68754fcc6722f61e9bff5fefa2fe49e4a
첼리 <kutochess1@gmail.com>
2025-06-11 13:58:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GlobalIT_FinalProject
+
+파이널 프로젝트
---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const MainPage = () => {
+  return (
+    <div id="wrap">
+      <div>MainPage</div>
+      <Link to={"/community"}>커뮤니티 게시판</Link>
+    </div>
+  );
+};
+
+export default MainPage;
